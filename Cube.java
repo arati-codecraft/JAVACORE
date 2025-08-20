6ee71009613874b6d1cbdbc90f5df5210150de2e
Arati Chavan <aratie2214@gmail.com>
2025-08-20 16:47:28 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Cube{
+               public static void main (String args[])
+              { 
+                 Scanner sc=new Scanner (System.in);
+                  System.out.println("enter the value");
+                    int a=sc.nextInt();
+                    System.out.println(" the cube is="+a*a*a);
+                    
+                }
+}
+
