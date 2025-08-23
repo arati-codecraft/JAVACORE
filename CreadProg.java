6ddf64508d4981c8e7e3eb4d6eafb80661bfeffb
Arati Chavan <aratie2214@gmail.com>
2025-08-23 16:22:07 +0530

    Add files via upload

---
+++ CreadProg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class CreadProg
+{
+      public static void main(String args[])
+      {
+         Scanner sc=new Scanner(System.in);
+         System.out.println(" Enter the score to access the cread  program");
+          int x= sc.nextInt();
+          
+           System.out.println("the score was");
+              
+               if (x>750||x==750)
+                  {
+                        
+                      System.out.println("YES can acess the cread program");
+                   }
+                  else 
+                  {
+                     
+                     System.out.println(" NO  ");
+                      
+                   }
+                
+              }
+}
